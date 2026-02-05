9210fb878aa6df4a6535c50097d9dbbbc0ea46f7
Shallom Otniel Christofel Lase <shallomoc01lase@gmail.com>
2026-02-05 21:29:46 +0700

    feat: add Jadwal model and clean main.dart

---
+++ lib/models/jadwal.dart
@@ -0,0 +1,34 @@
+class Jadwal {
+  String title;
+  String date;
+  String category;
+  int priority;
+  bool isDone;
+
+  Jadwal(this.title, this.date, this.category, this.priority, this.isDone);
+
+  void markAsDone() {
+    isDone = true;
+  }
+
+  String getPriorityLabel() {
+    switch (priority) {
+      case 1:
+        return '⚪ Super Low';
+      case 2:
+        return '🟢 Low';
+      case 3:
+        return '🟡 Medium';
+      case 4:
+        return '🟠 Important';
+      case 5:
+        return '🔴 Very Important';
+      default:
+        return 'Not a Priority';
+    }
+  }
+
+  String getStatusLabel() {
+    return isDone ? '✅ Selesai' : '⌛ Belum selesai';
+  }
+}
